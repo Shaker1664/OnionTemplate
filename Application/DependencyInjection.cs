32111e909bb8fb38464eea441eb1b0bb859046c8
Shaker1664 <shakerkamal@iut-dhaka.edu>
2021-02-11 20:14:04 +0600

    Generic Onion Template

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
